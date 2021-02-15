c768e44639acd2312fdfb134e9df776595421314
yazilim-rgb <77300294+yazilim-rgb@users.noreply.github.com>
2021-02-15 20:30:40 +0300

    X değerinin çift veya tek olduğunu bulmak için..

---
+++ TekBulma.py
@@ -0,0 +1,11 @@
+a = 1
+while a == 1:
+    sayı = int(input("-Girilen Sayının Çift Veya Tek Olduğunu Bulmam İçin Bana Sayısal Bir Değer Ver :)\n-Sayısal Değer;"))
+    if(sayı%2 ==0):
+        print("\nGirdiğin Sayının Çift Olsuğuna Yemin Edebilirim Ama Kanıtlayamam :)")
+        print(" ")
+        print(" ")
+    else:  
+        print("\nSayın Kesinlikle Çift Değil Kankaa :))")   
+        print(" ")
+        print(" ")
