86d4c38afd52b5a88541edd8d778133acd0d966d
yazilim-rgb <tektus6@gmail.com>
2021-02-15 11:35:54 +0300

    Proje Bitti Ve Kaydedildi

---
+++ İlk Uygulama.py
@@ -0,0 +1,4 @@
+isim_soyisim= input("-İsminizi Büyük Ve Küçük Harf İle Yazdırmak İçin Lütfen İsim Ve Soyisminizi Girin..\n-İsim Ve Soyisim;")
+print(isim_soyisim.upper())
+print(isim_soyisim.lower())
+print("Devoloped By Kamil :)")
