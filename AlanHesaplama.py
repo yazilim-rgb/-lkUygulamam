0be549b9953fa7b23d9b6ee53af3db4b3f11b875
yazilim-rgb <77300294+yazilim-rgb@users.noreply.github.com>
2021-02-18 01:06:47 +0300

    Hatalar Giderildi ;)

--- AlanHesaplama.py
+++ AlanHesaplama.py
@@ -1,5 +1,4 @@
-a=1
-while a==1:
-    sayı = int(input("Daire Alanını Bulmak İçin Lütfen Sayısal R Değerini Giriniz\n R="))
-    pi = 3.14
+pi=3.14
+while true:
+    sayi = input("Daire Alanını Bulmak İçin Lütfen Sayısal R Değerini Giriniz\n R="))
     print(int(sayı)**2*pi)
