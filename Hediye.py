d10bbe5cb4a99ffd2f7ad9fe4df245b88d6e3897
yazilim-rgb <77300294+yazilim-rgb@users.noreply.github.com>
2021-02-18 01:08:25 +0300

    Hatalar Giderildi..

--- Hediye.py
+++ Hediye.py
@@ -2,12 +2,11 @@ from time import sleep
 import colorama
 from colorama import  Fore,Back,Style
 colorama.init()
-a = 1
 print(Fore.CYAN)
 isim = input("İsmini Girsene Sana Bir Süprizim Olucak :))\n-İsminiz Lütfen;")
 print(Fore.YELLOW)
 print(isim+" Süpriz Yükleniyor....")
-while a ==1:
+while true:
     print(Fore.BLUE)
     print(isim.upper()+" Seni Kandırdım Ağla Moruk Hahahahahaha")
     sleep(0.5)
