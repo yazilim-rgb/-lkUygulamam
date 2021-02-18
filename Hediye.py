529a2ef5a5eb1fac394c909f42eeca84891a783c
yazilim-rgb <77300294+yazilim-rgb@users.noreply.github.com>
2021-02-18 08:47:00 +0300

    .

--- Hediye.py
+++ Hediye.py
@@ -6,7 +6,7 @@ print(Fore.CYAN)
 isim = input("İsmini Girsene Sana Bir Süprizim Olucak :))\n-İsminiz Lütfen;")
 print(Fore.YELLOW)
 print(isim+" Süpriz Yükleniyor....")
-while true:
+while True:
        print(Fore.BLUE)
        print(isim.upper()+" Seni Kandırdım Ağla Moruk Hahahahahaha")
        sleep(0.5)
